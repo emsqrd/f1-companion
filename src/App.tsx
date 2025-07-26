97ef904d3ad64e5f43be422194803f2d0c3018c8
Mark <madlyinanger37@gmail.com>
2025-07-26 16:38:16 -0500

    feat: adding teams cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { CircleUserRound } from 'lucide-react';
+import { Car, CircleUserRound } from 'lucide-react';
 
 import './App.css';
 import { Card, CardContent } from './components/ui/card';
 
 function App() {
   const drivers = ['Lando Norris', 'Oscar Piastri', 'Max Verstappen', 'Lewis Hamilton'];
+  const teams = ['Mercedes', 'McLaren', 'Red Bull Racing', 'Ferrari'];
 
   const driverCards = drivers.map((driver) => (
     <Card className="w-60 h-20">
@@ -15,9 +16,21 @@ function App() {
     </Card>
   ));
 
+  const teamCards = teams.map((team) => (
+    <Card className="w-60 h-20">
+      <CardContent className="flex items-center justify-center gap-2">
+        <Car />
+        {team}
+      </CardContent>
+    </Card>
+  ));
+
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 justify-center">{driverCards}</div>
+      <div className="space-y-12">
+        <div className="grid grid-cols-2 gap-4 justify-center">{driverCards}</div>
+        <div className="grid grid-cols-2 gap-4 justify-center">{teamCards}</div>
+      </div>
     </>
   );
 }
