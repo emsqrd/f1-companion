53ffc489489cbccdaa63f16df0378b37b24cb494
Mark <madlyinanger37@gmail.com>
2025-08-12 11:22:05 -0500

    feat: reworking layout of league component and display of teams. including a demo of potential layout that I got from copilot. also added new shadcn  components from demo example.

--- src/components/League/League.tsx
+++ src/components/League/League.tsx
@@ -1,22 +1,45 @@
 import { getTeams } from '@/services/teamService';
 import { Link } from 'react-router';
 
+import { Card, CardContent } from '../ui/card';
+
 export function League() {
   const teams = getTeams();
+
   return (
-    <div>
-      <ul>
+    <>
+      <div className="flex flex-col items-start pb-1">
+        <p className="font-semibold">League</p>
+        <h2 className="text-3xl font-semibold">COTA 2025</h2>
+      </div>
+      <hr className="border-border mb-4" />
+      <ol className="grid grid-cols-1 gap-2">
         {teams.map((team) => (
-          <li key={team.id} className="flex items-start">
-            <Link
-              to={`/team/${team.id}`}
-              className="text-blue-600 hover:text-blue-800 hover:underline"
-            >
-              {team.name}
+          <li key={team.id}>
+            <Link to={`/team/${team.id}`}>
+              <Card className="rounded-sm py-2">
+                <CardContent>
+                  <div className="flex flex-col items-start gap-1">
+                    <div className="flex items-center gap-2">
+                      <div className="text-muted-foreground font-mono text-sm">
+                        #
+                        {
+                          team.id //rank
+                        }
+                      </div>
+                      <div className="font-semibold">{team.name}</div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <div className="text-muted-foreground text-sm">Owner:</div>
+                      <div>{team.owner}</div>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
             </Link>
           </li>
         ))}
-      </ul>
-    </div>
+      </ol>
+    </>
   );
 }
--- src/services/teamService.ts
+++ src/services/teamService.ts
@@ -1,18 +1,17 @@
 import type { Team } from '@/contracts/Team';
 
-const teams: Team[] = [
-  {
-    id: 1,
-    name: 'Sainz of Trouble',
-  },
-  {
-    id: 2,
-    name: 'Chili Carbono',
-  },
-  {
-    id: 3,
-    name: 'Team 3',
-  },
+export const teams: Team[] = [
+  { id: 1, name: 'Team Redline Owner', owner: 'John' },
+  { id: 2, name: 'Luigi Ferrari', owner: 'Katharine' },
+  { id: 3, name: 'Chili Carbono', owner: 'Heidi' },
+  { id: 4, name: 'DayAfter Ricciardo', owner: 'Jessica' },
+  { id: 5, name: 'New Sainz of Hope', owner: 'Mark' },
+  { id: 6, name: 'Vroom vroom', owner: 'Kyle' },
+  { id: 7, name: 'Mindys team', owner: 'Mindy' },
+  { id: 8, name: 'Il Vecchio Drogo', owner: 'Austin' },
+  { id: 9, name: 'Wayne Dennison', owner: 'Wayne' },
+  { id: 10, name: 'VerStopHim', owner: 'Jeremy' },
+  { id: 11, name: 'I Saw The Sainz', owner: 'Ryan' },
 ];
 
 export function getTeams(): Team[] {
