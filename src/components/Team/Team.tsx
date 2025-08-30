05944e75a47de237ebf83e113c3c7d2e6b8292fa
Mark <madlyinanger37@gmail.com>
2025-08-30 14:53:23 -0500

    refactor: making responsive for larger screens.

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -18,7 +18,7 @@ export function Team() {
   }
 
   return (
-    <div className="mx-auto p-4 sm:p-8">
+    <div className="mx-auto w-full max-w-4xl p-4 sm:p-8 lg:px-8">
       <header className="mb-4">
         <nav aria-label="Breadcrumb" className="mb-4">
           <Link
@@ -30,59 +30,61 @@ export function Team() {
           </Link>
         </nav>
 
-        <div className="bg-card mb-8 space-y-4 rounded-2xl border-1 py-4">
-          <div className="flex w-full justify-center pb-2">
-            <h1 className="text-3xl font-bold">{team.name}</h1>
-          </div>
-
-          <div className="flex flex-row justify-center gap-12">
-            <div className="flex flex-col items-center">
-              <p className="text-muted-foreground font-medium">Budget</p>
-              <h1 className="text-lg font-bold">$200k</h1>
+        <div className="gap-4 sm:grid sm:grid-cols-2">
+          <div className="bg-card mb-6 flex flex-col justify-center space-y-4 rounded-2xl border-1 py-4 sm:mb-0">
+            <div className="flex justify-center">
+              <h1 className="text-3xl font-bold">{team.name}</h1>
             </div>
-            <div className="flex flex-col items-center">
-              <p className="text-muted-foreground font-medium">Trades</p>
-              <h1 className="text-lg font-bold">3/3</h1>
-            </div>
-          </div>
-        </div>
 
-        <div className="mb-4 flex flex-col space-y-4">
-          <Select defaultValue="round15">
-            <SelectTrigger className="h-auto min-h-[60px] w-full py-8 [&>span]:block [&>span]:w-full [&>span]:text-left">
-              <SelectValue placeholder="Pick a race" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="round15">
-                <div>
-                  <p className="text-muted-foreground font-medium">Round 15</p>
-                  <h1 className="text-2xl font-bold">Netherlands</h1>
-                </div>
-              </SelectItem>
-              <SelectItem value="round14">
-                <div>
-                  <p className="text-muted-foreground font-medium">Round 14</p>
-                  <h1 className="text-2xl font-bold">Hungary</h1>
-                </div>
-              </SelectItem>
-            </SelectContent>
-          </Select>
-
-          <Card className="gap-2 py-2">
-            <CardTitle className="pb-2 text-center text-xl font-bold">Round Results</CardTitle>
-            <CardContent>
-              <div className="flex justify-center gap-12">
-                <div className="text-center">
-                  <p className="text-muted-foreground font-medium">Finished</p>
-                  <h1 className="text-lg font-bold">1st</h1>
-                </div>
-                <div className="text-center">
-                  <p className="text-muted-foreground font-medium">Points</p>
-                  <h1 className="text-lg font-bold">679</h1>
-                </div>
+            <div className="grid grid-cols-2 gap-4 sm:flex sm:flex-row sm:justify-center">
+              <div className="flex flex-col items-center">
+                <p className="text-muted-foreground font-medium">Budget</p>
+                <h1 className="text-lg font-bold">$200k</h1>
               </div>
-            </CardContent>
-          </Card>
+              <div className="flex flex-col items-center">
+                <p className="text-muted-foreground font-medium">Trades</p>
+                <h1 className="text-lg font-bold">3/3</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-4 flex flex-col space-y-4 sm:mb-0">
+            <Select defaultValue="round15">
+              <SelectTrigger className="h-auto min-h-[60px] w-full py-8 [&>span]:block [&>span]:w-full [&>span]:text-left">
+                <SelectValue placeholder="Pick a race" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="round15">
+                  <div>
+                    <p className="text-muted-foreground font-medium">Round 15</p>
+                    <h1 className="text-2xl font-bold">Netherlands</h1>
+                  </div>
+                </SelectItem>
+                <SelectItem value="round14">
+                  <div>
+                    <p className="text-muted-foreground font-medium">Round 14</p>
+                    <h1 className="text-2xl font-bold">Hungary</h1>
+                  </div>
+                </SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Card className="gap-2 py-2">
+              <CardTitle className="pb-2 text-center text-xl font-bold">Round Results</CardTitle>
+              <CardContent>
+                <div className="grid grid-cols-2 gap-4 sm:flex sm:justify-center sm:gap-8 lg:gap-12">
+                  <div className="text-center">
+                    <p className="text-muted-foreground font-medium">Finished</p>
+                    <h1 className="text-lg font-bold">1st</h1>
+                  </div>
+                  <div className="text-center">
+                    <p className="text-muted-foreground font-medium">Points</p>
+                    <h1 className="text-lg font-bold">679</h1>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </header>
       <Tabs defaultValue="drivers">
